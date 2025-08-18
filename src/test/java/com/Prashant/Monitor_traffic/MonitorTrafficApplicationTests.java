9db0296220e61b215ea0018cb20837cfb14b7ebb
Prashant Vasukar <prashantvasukar@gmail.com>
2025-08-18 15:22:40 +0530

    initial commit

---
+++ src/test/java/com/Prashant/Monitor_traffic/MonitorTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prashant.Monitor_traffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorTrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
